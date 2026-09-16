f8b6fd5c35c14c38af73172cdde6955ddab32cc3
Yuji Yoshida <86038476+d-wedding@users.noreply.github.com>
2026-09-16 12:30:32 +0900

    Add files via upload

--- site-config.js
+++ site-config.js
@@ -18,7 +18,7 @@ window.SITE_CONFIG = {
     history: "./assets/location.jpg",
     prewedding: "./assets/prewedding.jpg",
     location: "./assets/history.jpg",
-    opm: "./assets/opm.jpg"
+    opm: "./assets/opm-20260916.jpg"
   },
   fonts: {
     japanese: '"Yu Mincho", "YuMincho", "Hiragino Mincho ProN", "Noto Serif JP", serif',
